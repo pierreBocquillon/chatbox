ac5422153f4cd06557ae7ba206afd1ee7a76b16c
BOCQUILLON pierre <pierre.bocquillon@hetic.net>
2019-03-22 00:41:48 +0100

    chat box v0.8

--- js/todo.js
+++ js/todo.js
@@ -13,10 +13,10 @@ document.addEventListener('DOMContentLoaded', ()=> {
     Gestion de la donnée
     */
         // Définir la BDD PouchDB
-        const pouchDatabase = new PouchDB('chat');
+        pouchDatabase = new PouchDB('todo');
 
         // Définir la BDD CouchDB
-        const couchDatabase = 'http://134.209.231.153:5984/chat';
+        const couchDatabase = 'http://134.209.231.153:5984/todo';
 
         // Création d'une méthode pour synchroniser PouchDB et CouchDB
         const syncDatabases = () => {
@@ -30,13 +30,27 @@ document.addEventListener('DOMContentLoaded', ()=> {
         const fetchMessages = () => {
             messages.innerHTML = ''
             pouchDatabase.allDocs( { include_docs: true }, ( err, data ) => {
-                err ? console.error(err) : data.rows.map( item =>  messages.innerHTML += `<p class="${item.doc.author}"><b>${item.doc.author} :</b><br> ${item.doc.content}</p><hr>` ) ;
+                err ? console.error(err) : data.rows.map( item => messages.innerHTML += `<p class="${item.doc.status}" id="${item.doc._id}"  > <b>${item.doc.author}:</b> ${item.doc.content}<input class="donebtn btn" type="button" value="Done" style="margin-left: auto;" onclick="donetodo('${item.doc._id}')"></input>  <input class="undonebtn btn" type="button" value="Undone" onclick="undonetodo('${item.doc._id}')"></input> <input class="deletebtn btn" type="button" value="delete" onclick="deletetodo('${item.doc._id}')"></input></p>` ) ;
+                
                 for(var i= 0; i < document.getElementsByClassName(document.cookie).length; i++)
                 {
                     //document.getElementsByClassName(me)[i].style.backgroundColor = "#3a39ad";
                     //document.getElementsByClassName(me)[i].style.alignSelf = "flex-end";
                 }
-                console.clear();
+
+                btn1 = document.getElementsByClassName("donebtn")
+                btn2 = document.getElementsByClassName("undonebtn")
+                btn3 = document.getElementsByClassName("deletebtn")
+
+                if(document.cookie != "tazertazer"){
+                    for(var i= 0; i < btn1.length; i++){
+                        btn1[i].style.display = "none";
+                        btn2[i].style.display = "none";
+                        btn3[i].style.display = "none";
+                    }
+                }
+
+
                 messages.scrollTo(0,document.querySelector("#messages").scrollHeight);
             });
         };
@@ -63,8 +77,11 @@ document.addEventListener('DOMContentLoaded', ()=> {
                 _id: dateNow,
                 author: document.cookie,
                 content: message,
+                status: "todo",
                 created_at: dateNow
             })
+            .then( success => console.log(success) )
+            .catch( err => console.error(err) );
         }
 
         
@@ -97,3 +114,36 @@ document.addEventListener('DOMContentLoaded', ()=> {
     //
 
 });
+
+
+const donetodo = ( id ) => {
+    pouchDatabase.get(id).then(function (doc) {
+        doc.status = "done";
+        return pouchDatabase.put(doc);
+      }).then(function () {
+        return pouchDatabase.get(id);
+      }).then(function (doc) {
+        console.log(doc);
+      });
+      
+}
+const undonetodo = ( id ) => {
+    pouchDatabase.get(id).then(function (doc) {
+        doc.status = "todo";
+        return pouchDatabase.put(doc);
+      }).then(function () {
+        return pouchDatabase.get(id);
+      }).then(function (doc) {
+        console.log(doc);
+      });
+ 
+    
+}
+const deletetodo = ( id ) => {
+    pouchDatabase.get(id).then(function (doc) {
+        return pouchDatabase.remove(doc);
+      });
+}
+
+
+       
